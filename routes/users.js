a7f7a2554e0869b7c102835dcf2212a58154fa8b
Aleksandra Nazhestkina <splinatus@yandex.ru>
2023-01-06 20:52:53 +0300

    добавить аворизацию и логгер

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,12 @@
 const router = require('express').Router();
+const auth = require('../middlewares/auth');
 
 const {
   getUserInfo,
   updateUser,
 } = require('../controllers/users');
+
+router.use(auth);
 
 router.get('/users/me', getUserInfo);
 router.patch('/users/me', updateUser);
