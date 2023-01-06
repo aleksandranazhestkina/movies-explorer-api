34fa3a6be6ac78c5442b020dddc8afdf2caa9d12
Aleksandra Nazhestkina <splinatus@yandex.ru>
2023-01-06 20:59:20 +0300

    добавить контроллер фильмов

---
+++ controllers/movies.js
@@ -0,0 +1,70 @@
+const Movie = require('../models/movie');
+const NotFoundError = require('../errors/not-found-error');
+const BadRequestError = require('../errors/bad-request-error');
+const ForbiddenError = require('../errors/forbidden-error');
+
+module.exports.getMovies = (req, res, next) => {
+  Movie.find({})
+    .then((cards) => res.send(cards.reverse()))
+    .catch(next);
+};
+
+module.exports.createMovie = (req, res, next) => {
+  const {
+    country,
+    director,
+    duration,
+    year,
+    description,
+    image,
+    trailerLink,
+    thumbnail,
+    movieId,
+    nameRU,
+    nameEN,
+  } = req.body;
+  Movie.create({
+    country,
+    director,
+    duration,
+    year,
+    description,
+    image,
+    trailerLink,
+    thumbnail,
+    movieId,
+    nameRU,
+    nameEN,
+    owner: req.user._id,
+  })
+    .then((card) => res.send(card))
+    .catch((err) => {
+      if (err.name === 'ValidationError') {
+        next(new BadRequestError('Переданы некорректные данные при создании фильма.'));
+      } else {
+        next(err);
+      }
+    });
+};
+
+module.exports.deleteCard = (req, res, next) => {
+  const { filmId } = req.params;
+  Movie.findById(filmId)
+    .orFail(() => next(new NotFoundError('Фильм с указанным _id не найден.')))
+    .then((film) => {
+      if (!film.owner.equals(req.user._id)) {
+        next(new ForbiddenError('Попытка удалить чужой фильм.'));
+      }
+      return film.remove();
+    })
+    .then(() => {
+      res.send({ message: `Фильм ${filmId} удален` });
+    })
+    .catch((err) => {
+      if (err.name === 'CastError') {
+        next(new BadRequestError('Переданы некорректные данные при удалении фильма.'));
+      } else {
+        next(err);
+      }
+    });
+};
